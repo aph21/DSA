182f393b28f61b642971104230995f058f26f7a8
Anjanaph21 <anjana@wiwitness.com>
2025-11-07 10:39:54 +0530

    feat: add number swapping demo and update gitignore
    - Add .gitignore to exclude node_modules folder
    - Create basic HTML template in Day_1/index.html
    - Implement number swapping without temporary variable in Day_1/app.js

--- Day_1/app.js
+++ Day_1/app.js
@@ -1,1 +1,31 @@
-print(7+2)
+// // print "Hello world" without using console.log
+
+// //Method 1: using alert()
+// alert("Hello World!");
+
+// //Mathod 2: using document.write()
+// document.write("Hello World");
+
+// //Method 3: using process.stdout.write()
+// process.stdout.write("Hello World");
+
+// //Method 4: using function
+
+// function sayHello() {
+//     return "Hello world!";
+// }
+
+// const msg = sayHello();
+// console.log(msg);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//2 Swap two numbers without using a third variable
+const prompt = require("prompt-sync")()
+let a = Number(prompt("Enter a:"));
+let b = Number(prompt("Enter b:"))
+console.log("a and b before swapping: ", a, b);
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("a and b after swapping: ", a, b)
