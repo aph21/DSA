d37949ffcd48a533cc41c02cd9bccee38e9dd53a
Anjanaph21 <anjana@wiwitness.com>
2025-11-11 20:55:51 +0530

    2 Swap two numbers without using a third variable

--- Day_1/app.js
+++ Day_1/app.js
@@ -1,3 +1,4 @@
+const prompt = require("prompt-sync")()
 // // print "Hello world" without using console.log
 
 // //Method 1: using alert()
@@ -21,11 +22,24 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //2 Swap two numbers without using a third variable
-const prompt = require("prompt-sync")()
+
+//method 1: using arithmetic operators
+
+// let a = Number(prompt("Enter a:"));
+// let b = Number(prompt("Enter b:"))
+// console.log("a and b before swapping: ", a, b);
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log("a and b after swapping: ", a, b)
+
+
+//Method 2: using bitwise XOR operator
+
 let a = Number(prompt("Enter a:"));
 let b = Number(prompt("Enter b:"))
 console.log("a and b before swapping: ", a, b);
-a = a + b;
-b = a - b;
-a = a - b;
-console.log("a and b after swapping: ", a, b)
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+console.log("a and b after swapping: ", a, b);
